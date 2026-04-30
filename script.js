5a3e56750a3084c0a6dd998f771e18503dddc6dc
bhoomibalegar-sys <bhoomibalegar@gmail.com>
2026-04-30 19:50:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
+function showTab(tab) {
+    document.getElementById("analyzer").style.display = "none";
+    document.getElementById("generator").style.display = "none";
+    document.getElementById(tab).style.display = "block";
+}
+
 function checkStrength() {
-    fetch("analyzer.php", {
+    let password = document.getElementById("password").value;
+
+    if (password === "") {
+        alert("Enter password first");
+        return;
+    }
+
+    fetch("http://localhost/password_tool/analyzer.php", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         },
-        body: "password=test123"
+        body: "password=" + encodeURIComponent(password)
     })
-    .then(res => res.text())
+    .then(response => response.text())
     .then(data => {
-        alert("Response: " + data);
+        console.log("Response:", data);   // DEBUG
+        document.getElementById("result").innerHTML = data;
     })
-    .catch(err => {
-        alert("Error: " + err);
+    .catch(error => {
+        console.error("Fetch Error:", error);
+    });
+}
+
+function generateWordlist() {
+    let name = document.getElementById("name").value;
+
+    if (name === "") {
+        alert("Enter name");
+        return;
+    }
+
+    let data = new URLSearchParams();
+    data.append("name", name);
+    data.append("dob", document.getElementById("dob").value);
+    data.append("pet", document.getElementById("pet").value);
+    data.append("company", document.getElementById("company").value);
+
+    fetch("http://localhost/password_tool/generator.php", {
+        method: "POST",
+        body: data
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log("Response:", data);   // DEBUG
+        document.getElementById("genResult").innerHTML = data;
+    })
+    .catch(error => {
+        console.error("Fetch Error:", error);
     });
 }
